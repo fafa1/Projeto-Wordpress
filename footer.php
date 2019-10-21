8e911ae5b558912ba098050698de888ac8fecf7a
fafa1 <bfagner@hotmail.com>
2019-10-20 21:37:48 -0300

    projeto-final

--- footer.php
+++ footer.php
@@ -1,15 +1,18 @@
 <footer>
 	<nav class="nav-footer">
-		<ul>
-			<li class="current_page_item"><a href="Menu da Semana">Menu</a></li>
-			<li><a href="sobre">Sobre</a></li>
-			<li><a href="contato">Contato</a></li>
-		</ul>
+	<?php
+				$args = array(
+					'menu' => 'principal',
+					'container' => false
+				);
+				wp_nav_menu( $args );
+			?>
 	</nav>
 
-	<p>Rest © 2015. Alguns direitos reservados.</p>
+	<p><?php bloginfo('name') ?> © <?php echo date('Y'); ?>. Alguns direitos reservados.</p>
 </footer>
 
 </body>
 <?php wp_footer(); ?>
+
 </html>
--- header.php
+++ header.php
@@ -1,30 +1,36 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-	<head>
-		<meta charset="utf-8">
-		<title><?php bloginfo('name'); ?></title>
 
-		<link href='https://fonts.googleapis.com/css?family=Alegreya+SC' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri();?>/style.css">
-    <!-- header wordpress -->
-    <?php wp_head(); ?> 
-    <!-- header wordpress -->
-	</head>
+<head>
+	<meta charset="utf-8">
+	<title><?php bloginfo('name'); ?> |<?php wp_title(''); ?> | <?php the_field('title_seo'); ?></title>
 
-	<body>
-		<header>
-			<nav>
-				<ul>
-					<li class="current_page_item"><a href="Menu da Semana">Menu</a></li>
-					<li><a href="sobre">Sobre</a></li>
-					<li><a href="contato">Contato</a></li>
-				</ul>
-			</nav>
+	<meta name="description" content="<?php bloginfo('name'); ?> | <?php wp_title(); ?> |
+	<?php the_field('description_seo'); ?>">
 
-			<h1><img src="<?php echo get_stylesheet_directory_uri();?>/img/rest.png" alt="Rest"></h1>
+	<link href='https://fonts.googleapis.com/css?family=Alegreya+SC' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
+	<!-- header wordpress -->
+	<?php wp_head(); ?>
+	<!-- header wordpress -->
+</head>
 
-			<?php $contato = get_page_by_title('contatos') ?>
-			
-			<p><?php the_field('endereco_header', $contato)?></p>
-			<p class="telefone"><?php the_field('telefone_header', $contato)?></p>
-		</header>
+<body>
+	<header>
+		<nav>
+			<?php
+				$args = array(
+					'menu' => 'principal',
+					'container' => false
+				);
+				wp_nav_menu( $args );
+			?>
+		</nav>
+
+		<h1><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/rest.png" alt="Rest"></h1>
+
+		<?php $contato = get_page_by_title('contatos') ?>
+
+		<p><?php the_field('endereco_header', $contato) ?></p>
+		<p class="telefone"><?php the_field('telefone_header', $contato) ?></p>
+	</header>
